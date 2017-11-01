6d63a08f45da8c7ea082d3c062fc45647d84a197
wangdong <819743020@qq.com>
2017-11-02 05:21:15 +0800

    KEY

--- IPC/sem/create_sem.c
+++ IPC/sem/create_sem.c
@@ -1,11 +1,15 @@
 #include"ipc.h"
 #include<sys/sem.h>
-int main()
-{
-	int semid;
-	key_t key=Ftok("mytest",0xfe);//create key
+int main(int argc,char*argv[])
+{	
+	if(argc !=3)
+	{
+		printf("<Usage >:pathname %s proj_id",argv[0]);
+	}
+
+	key_t key=Ftok(argv[1],atio(argv[2]));//create key
 	printf("semkey=%x\n",key);
-	semid=Semget(key,1,IPC_CREAT|IPC_EXCL|0755);
+	int	semid=Semget(key,1,IPC_CREAT|IPC_EXCL|0755);
 	printf("semid=%d\n",semid);	
 	return 0;
 }
--- IPC/sem/remove_sem.c
+++ IPC/sem/remove_sem.c
@@ -1,6 +1,7 @@
 #include"ipc.h"
 int main()
 {
+	
 	key_t key=Ftok("mytest",0xfe);
 	int semid=Semget(key,0,0);
 	semctl(semid,0,IPC_RMID);
